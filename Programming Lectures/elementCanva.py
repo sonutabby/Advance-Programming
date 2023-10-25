e928e6478c7bd0f2963eb084e4bde9305c80d282
sonutabby <avril.fernandez.6921@gmail.com>
2023-10-25 20:49:23 +0400

    Create elementCanva.py

---
+++ Programming Lectures/elementCanva.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+
+canvas = Canvas(root, width=1920, height=1080)
+canvas.pack()
+
+line = canvas.create_line(50, 70, 100, 70, fill="red", width=2)
+rectangle = canvas.create_rectangle(70, 80, 100, 110, fill="blue",outline="Black", width=2)
+oval = canvas.create_oval(170, 180, 300, 310, fill="red", width=2)
+canvas.moveto(oval, 50, 100)
+text = canvas.create_text(40, 30, text="What if ako nalang", font=('Arial', 14), fill="green")
+canvas.moveto(text, 50, 100)
+
+root.mainloop()
