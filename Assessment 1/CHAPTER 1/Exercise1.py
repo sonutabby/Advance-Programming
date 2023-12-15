a965f6be5803c7be8797be10cb3419b4cffaad14
sonutabby <avril.fernandez.6921@gmail.com>
2023-12-15 10:36:07 +0400

    Initial Commit

--- Assessment 1/CHAPTER 1/Exercise1.py
+++ Assessment 1/CHAPTER 1/Exercise1.py
@@ -1,9 +1,10 @@
+#ADDING GREETINGS TO THE USER
 print ("Hello, user!")
 user_name = input ("What is your name?\n")
 age = int (input ("What is your age?\n"))
 print ("It is good to meet you, " + user_name)
 
-username_length = len(user_name)
+username_length = len(user_name) #length of username
 print("The length of your name is:")
 print(username_length)
 
